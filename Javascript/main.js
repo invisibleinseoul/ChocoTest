fcad89f67bbe558f04da3b0a35645fcefd210844
Rowan-K <56216751+Rowan-K@users.noreply.github.com>
2022-01-14 12:42:48 +0900

    ChocoTest
    Edit forumQuestion.html, main.css, main.js

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,3 +1,4 @@
+//Selected Navigation > Change font color
 const navMenuEl = document.querySelector('.nav__menu')
 navMenuEl.addEventListener('click', function(){
     navMenuEl.classList.add('clicked');
@@ -6,53 +7,8 @@ navMenuEl.addEventListener('click', function(){
     }
 });
 
-$(document)
-  .ready(function (){
-    let tag = {};
-    let counter = 0;
-
-    function addTag(value){
-      tag[counter] = value;
-      counter++;
-    }
-    function marginTag(){
-      return Object.values(tag)
-        .filter(function (word){
-          return word !== "";
-        });
-    }
-
-    $("#tag")
-      .on("keyup", function (e){
-        let self = $(this);
-        console.log("keypress");
-
-        if(e.key === "Enter" || e.keyCode == 32){
-          let tagValue = self.val();
-          if(tagValue !== ""){
-            let result = Object.values(tag)
-              .filter(function (word){
-                return word === tagValue;
-              })
-            if(result.length == 0){
-              $("tag-list")
-              .append("<li class='tag-item'>" + tagValue + "<span class='del-btn' idx='" + counter + "'>x</span></li>");
-              addTag(tagValue);
-              self.val("");
-            } else {
-              alert("태그값이 중복됩니다.");
-            }
-          }
-          e.preventDefault();
-        }
-      });
-$(document)
-  .on("click", ".del-btn", function(e){
-    let index = $(this)
-      .attr("idx");
-    tag[index] = "";
-    $(this)
-      .parent()
-      .remove();
-  });
+//forumQuest.html > Error Code > Auto Height
+$("textarea.ta__eCode").on('keydown keyup', function(){
+  $(this).height(1).height($(this).prop('scrollHeight')+1);
 })
+
