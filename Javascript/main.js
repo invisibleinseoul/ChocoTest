c95ec6598ccffc84a91656d246cd542e20f10190
Rowan-K <56216751+Rowan-K@users.noreply.github.com>
2022-01-17 03:37:44 +0900

    ChocoTest
    Edit main.js
    현재 날짜로 표시 but 한 페이지 안에 날짜 중복 표시 안 됨

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,14 +1,28 @@
-//Selected Navigation > Change font color
-const navMenuEl = document.querySelector('.nav__menu')
-navMenuEl.addEventListener('click', function(){
-    navMenuEl.classList.add('clicked');
-    if(navMenuEl.classList.contains('clicked')){
-      navMenuEl.classList.remove('clicked')
-    }
-});
+//Navigation Change font color  
+//동작되나 새로고침 되면 유지되지 않음
+function changeColor(){
+  document.getElementById("nav__menu__li").style.color = "#FA3D4B";
+  window.location.reload(true);
+  console.log("change!");
+}
+
 
 //forumQuest.html > Error Code > Auto Height
 $("textarea.ta__eCode").on('keydown keyup', function(){
   $(this).height(1).height($(this).prop('scrollHeight')+1);
 })
 
+
+//Display Date
+const el = document.getElementById("date");
+
+function clock(){
+  const time = new Date();
+  const year = time.getFullYear();
+  const month = time.getMonth() + 1;
+  const date = time.getDate();
+
+  el.innerText =
+  `${year}.${("00" + month.toString()).slice(-2)}.${("00" + date.toString()).slice(-2)}`  //Display in double digits
+}
+clock();
