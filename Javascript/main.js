4ab107906bda05652d2df69387d39838de8cd1f0
HJ Kim <56216751+Rowan-K@users.noreply.github.com>
2022-01-18 12:01:31 +0900

    ChocoTest
    Edit CKeditor
    A problem that the height cannot be controlled.
    A problem that some plug-ins cannot be inserted.
    >> Need to fix

--- Javascript/main.js
+++ Javascript/main.js
@@ -84,11 +84,13 @@ ClassicEditor
 		window.editor = editor;
 	} )
 	.catch( error => {
-		console.error( 'Oops, something went wrong!' );
 		console.error( 'Please, report the following error on https://github.com/ckeditor/ckeditor5/issues with the build id and the error stack trace:' );
-		
 		console.error( error );
 	} );
+
+	ClassicEditor.replace('editor', {
+		height: 300
+	});
 
 
 
